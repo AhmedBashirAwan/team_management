753bb58d75da29cd537a8860c93c11c2dce8fe92
Ahmed <ahmedbsahirawan@gmail.com>
2023-08-02 17:49:56 +0500

    projects done

--- lib/src/chating/components/teamlist.dart
+++ lib/src/chating/components/teamlist.dart
@@ -1,17 +1,47 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:team_management/customised/widgets/bottomnavigation.dart';
 import 'package:team_management/src/auth/register/register.dart';
 import '../../Dashboard/screen/dashboard.dart';
 
 class TeamList extends StatefulWidget {
-  const TeamList({Key? key}) : super(key: key);
+  final List<String>? selectedMembers;
+  TeamList({Key? key, this.selectedMembers}) : super(key: key);
 
   @override
   State<TeamList> createState() => _TeamListState();
 }
 
 class _TeamListState extends State<TeamList> {
+  List<String> teamMembers = [];
+  List<Map<String, dynamic>> membersInfo = [];
+
+  @override
+  void initState() {
+    super.initState();
+    teamMembers = widget.selectedMembers ?? [];
+    getTeamMembersInfo();
+  }
+
+  Future<void> getTeamMembersInfo() async {
+    membersInfo.clear();
+
+    for (String memberId in teamMembers) {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('userData')
+          .where('userId', isEqualTo: memberId)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        membersInfo.add(querySnapshot.docs[0].data() as Map<String, dynamic>);
+      } else {
+        print("Member with ID $memberId does not exist.");
+      }
+    }
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,111 +84,86 @@ class _TeamListState extends State<TeamList> {
               ),
             ),
           ),
-          FutureBuilder<List<Map<String, dynamic>>>(
-            // future: TeamList.getUsersList(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text('Error occurred'),
-                );
-              }
-
-              List<Map<String, dynamic>> userDataList = snapshot.data ?? [];
-
-              return SizedBox(
-                height: MediaQuery.of(context).size.height * 0.73,
-                child: ListView.builder(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  physics: const ClampingScrollPhysics(),
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemCount: userDataList.length,
-                  itemBuilder: (context, index) {
-                    var userData = userDataList[index];
-
-                    return GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        margin: const EdgeInsets.only(top: 10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        height: MediaQuery.of(context).size.height * 0.13,
-                        child: Material(
-                          elevation: 5,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: Row(
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.73,
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              physics: const ClampingScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: membersInfo.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> memberData = membersInfo[index];
+                return GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    height: MediaQuery.of(context).size.height * 0.13,
+                    child: Material(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 50,
+                              margin: const EdgeInsets.only(left: 5),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(100),
+                                child: Image.network(
+                                  'https://i2-prod.gazettelive.co.uk/incoming/article20679579.ece/ALTERNATES/s615b/0_jwr_mga_260521willowgracecampbell.jpg',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                Container(
-                                  height: 50,
-                                  margin: const EdgeInsets.only(left: 5),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(100),
-                                    child: Image.network(
-                                      'https://i2-prod.gazettelive.co.uk/incoming/article20679579.ece/ALTERNATES/s615b/0_jwr_mga_260521willowgracecampbell.jpg',
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 15,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
+                                Row(
                                   children: [
-                                    Row(
-                                      children: [
-                                        Text(
-                                          userData['firstName'] ?? '',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 16,
-                                            color: Colors.black,
-                                          ),
-                                        ),
-                                        Text(
-                                          userData['lastName'] ?? '',
-                                          style: TextStyle(
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 16,
-                                            color: Colors.black,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: 3,
-                                    ),
                                     Text(
-                                      userData['email'] ?? '',
+                                      memberData['fullName'] ?? '',
                                       style: TextStyle(
                                         fontWeight: FontWeight.w500,
-                                        fontSize: 13,
+                                        fontSize: 16,
                                         color: Colors.black,
                                       ),
                                     ),
                                   ],
                                 ),
+                                SizedBox(
+                                  height: 3,
+                                ),
+                                Text(
+                                  memberData['email'] ?? '',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 13,
+                                    color: Colors.black,
+                                  ),
+                                ),
                               ],
                             ),
-                          ),
+                            Spacer(),
+                            IconButton(
+                                onPressed: () {}, icon: Icon(Icons.more_vert))
+                          ],
                         ),
                       ),
-                    );
-                  },
-                ),
-              );
-            },
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
